399936f2b8c9f183745457b06dfbc944e8884223
AHTepsilon <alehertoro725@gmail.com>
2021-09-10 08:05:11 -0500

    Fixed issue with IDs

--- app/src/main/java/com/example/quiz1/MainActivity.java
+++ app/src/main/java/com/example/quiz1/MainActivity.java
@@ -3,12 +3,17 @@ package com.example.quiz1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button toRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toRegister = findViewById(R.id.RegisterButton);
     }
 }
